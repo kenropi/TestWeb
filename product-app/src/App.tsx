d482fdfe312eb6a194fc04b06367dd87fe61ad87
kenropi <kennywilliam2005@gmail.com>
2026-01-14 00:48:32 +0700

    Update App.tsx

--- product-app/src/App.tsx
+++ product-app/src/App.tsx
@@ -1,7 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import { fetchProducts } from './api/products'
+import { useState } from 'react'
 
 function App() {
+  const [search, setSearch] = useState('')
+
   const { data, isLoading, error } = useQuery({
     queryKey: ['products'],
     queryFn: fetchProducts,
@@ -10,20 +13,37 @@ function App() {
   if (isLoading) return <p style={center}>Loading...</p>
   if (error) return <p style={center}>Error mengambil data</p>
 
+  const filteredData = data?.filter(p =>
+    p.title.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <div style={page}>
       <div style={container}>
         <h1 style={title}>Product List</h1>
 
+        {/* SEARCH */}
+        <input
+          type="text"
+          placeholder="Search product..."
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+          style={searchInput}
+        />
+
         <div style={card}>
           <ul style={list}>
-            {data?.map(p => (
+            {filteredData?.map(p => (
               <li key={p.id} style={item}>
                 <span>{p.title}</span>
                 <span>${p.price}</span>
               </li>
             ))}
           </ul>
+
+          {filteredData?.length === 0 && (
+            <p style={empty}>Product tidak ditemukan</p>
+          )}
         </div>
       </div>
     </div>
@@ -31,6 +51,8 @@ function App() {
 }
 
 export default App
+
+/* ===== STYLE ===== */
 
 const page = {
   minHeight: '100vh',
@@ -46,8 +68,19 @@ const container = {
 }
 
 const title = {
-  marginBottom: '20px',
+  marginBottom: '16px',
   textAlign: 'center' as const,
+}
+
+const searchInput = {
+  width: '100%',
+  padding: '10px 14px',
+  borderRadius: '8px',
+  border: '1px solid #1e293b',
+  marginBottom: '16px',
+  background: '#020617',
+  color: '#e5e7eb',
+  outline: 'none',
 }
 
 const card = {
@@ -67,6 +100,12 @@ const item = {
   justifyContent: 'space-between',
   padding: '12px 0',
   borderBottom: '1px solid #1e293b',
+}
+
+const empty = {
+  textAlign: 'center' as const,
+  padding: '16px',
+  color: '#94a3b8',
 }
 
 const center = {
